7cdc704021ddfe8ed8d0f14051bb7d92fc90f206
pythonart <31215688+pythonart@users.noreply.github.com>
2017-10-26 22:21:21 +0530

    Update urls.py

--- accountingbuddy/urls.py
+++ accountingbuddy/urls.py
@@ -10,5 +10,6 @@ urlpatterns=[
 url(r'^pricing/india/$',views.pricing_india,name='pricing-india'),
 url(r'^businessrequest/submit/$',views.businessRequestFormView,name='business-req'),
 url(r'^thankyou/$',TemplateView.as_view(template_name='thanks.html'),name='thanks'),
+url(r'^thankyou/$',TemplateView.as_view(template_name='thanks_support.html'),name='thanks_support'),
 url(r'^supportrequest/create/$',views.supportRequestView,name='suppport-req'),
 ]
