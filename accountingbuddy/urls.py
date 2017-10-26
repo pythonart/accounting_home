98ff339ea9850ac6d117169849c64c7215fa062d
pythonart <31215688+pythonart@users.noreply.github.com>
2017-10-26 23:13:44 +0530

    Update urls.py

--- accountingbuddy/urls.py
+++ accountingbuddy/urls.py
@@ -12,4 +12,5 @@ url(r'^businessrequest/submit/$',views.businessRequestFormView,name='business-re
 url(r'^thankyou/$',TemplateView.as_view(template_name='thanks.html'),name='thanks'),
 url(r'^thankyou/support$',TemplateView.as_view(template_name='thanks_support.html'),name='thanks_support'),
 url(r'^supportrequest/create/$',views.supportRequestCreate,name='suppport-req'),
+url(r'^supportrequest/view/$',views.SupportRequestView.as_view(),name='support-req-view'),  
 ]
