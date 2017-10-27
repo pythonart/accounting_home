ba56615552ce6f8631b5e932238f17c761282e9e
pythonart <31215688+pythonart@users.noreply.github.com>
2017-10-27 10:35:05 +0530

    Custom Field Search Included

--- business/manager_collect.py
+++ business/manager_collect.py
@@ -13,10 +13,7 @@ taxli=[] #List of TaxCode objects
 for tax in tcodes:
   taxli.append(TaxCode(tcodes[tax],tax))
 
-sinvli=[] # List of sales invoice objects
-for sinvoice in sinvoices:
-  sinvli.append(SalesInvoice(sinvoices[sinvoice],taxli))
-  
+
 custom_field_list=[] #List of Custom Fields.
 for custom_field in customFields:
   custom_field_list.append(CustomField(customFields[custom_field],custom_field))  
@@ -25,6 +22,9 @@ cli=[]  # List of customer objects
 for customer in customers:
   cli.append(CustomerDetails(customers[customer],custom_field_list))
   
+sinvli=[] # List of sales invoice objects
+for sinvoice in sinvoices:
+  sinvli.append(SalesInvoice(sinvoices[sinvoice],taxli,custom_field_list))  
 
 
             
