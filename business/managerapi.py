289dc626f65f04c5742de5172ace34f525a029bf
pythonart <31215688+pythonart@users.noreply.github.com>
2017-10-28 12:14:57 +0530

    Update managerapi.py

--- business/managerapi.py
+++ business/managerapi.py
@@ -204,6 +204,10 @@ class manager_object:
     def index_customfield(self):
         ''' Fetch all Custom Field Indexes'''
         return self.index_objects('CustomField')
+    
+    def index_businessdetails(self):
+        '''Fetch Business Details'''
+        return self.index_objects('BusinessDetails')
         
 # Below, fetching objects from a collection
     def get_object_thread(self, o_dict, index):
@@ -276,6 +280,10 @@ class manager_object:
     def get_customfields(self):
         ''' Get all Custom field details '''
         return self.get_objects(self.index_customfield())
+    
+    def get_businessdetails(self):
+        ''' Get all Business Details'''
+        return self.get_objects(self.index_businessdetails())
 
     # def get_sales_inventory_items(self):
     #     """Get all the sales inventory Items."""
