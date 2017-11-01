505255e31731b3ac40a9bd12d35ea03665db2d3f
pythonart <31215688+pythonart@users.noreply.github.com>
2017-11-01 15:08:16 +0530

    Update inbuilt_tax_codes.py

--- business/inbuilt_tax_codes.py
+++ business/inbuilt_tax_codes.py
@@ -1,5 +1,10 @@
-CGST_SGST_025_JSON={
-  "Name": "SGST",
+#Inbuilt Taxcode code converted to TaxCode objects
+
+#CGST + SGST 3%
+
+CGST_SGST_3_CODE="48565e06-c5be-4e8b-8610-ec44b2a34026"
+CGST_SGST_3_JSON={
+  "Name": "SGST 3%",
   "Components": [
     {
       "Name": "SGST",
@@ -14,4 +19,43 @@ CGST_SGST_025_JSON={
   "TaxRateType": "MultiRate"
 }
 
-CGST_SGST_025_CODE="48565e06-c5be-4e8b-8610-ec44b2a34026"
+#CGST + SGST 5%
+CGST_SGST_5_CODE="ff0e8c9e-ae08-4689-baef-7e9979cbd43b"
+CGST_SGST_5_JSON={
+  "Name": " SGST 5%",
+  "Components": [
+    {
+      "Name": "SGST",
+      "Rate": 2.5
+    },
+    {
+      "Name": "CGST",
+      "Rate": 2.5
+    }
+  ],
+  "TaxRate": "CustomRate",
+  "TaxRateType": "MultiRate"
+}
+
+
+#CGST + SGST 12%
+CGST_SGST_5_CODE="b1a06a8e-b84f-4f7f-8c0f-4fae535d24c2"
+CGST_SGST_5_JSON={
+  "Name": " SGST 12%",
+  "Components": [
+    {
+      "Name": "SGST",
+      "Rate": 6.0
+    },
+    {
+      "Name": "CGST",
+      "Rate": 6.0
+    }
+  ],
+  "TaxRate": "CustomRate",
+  "TaxRateType": "MultiRate"
+}
+
+
+
+
