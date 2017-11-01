24cb47b3c42a58b41e7d5b1714c07bcd479b236b
pythonart <31215688+pythonart@users.noreply.github.com>
2017-11-01 14:58:32 +0530

    Create inbuilt_tax_codes.py

---
+++ business/inbuilt_tax_codes.py
@@ -0,0 +1,17 @@
+CGST_SGST_025_JSON={
+  "Name": "SGST",
+  "Components": [
+    {
+      "Name": "SGST",
+      "Rate": 1.5
+    },
+    {
+      "Name": "CGST",
+      "Rate": 1.5
+    }
+  ],
+  "TaxRate": "CustomRate",
+  "TaxRateType": "MultiRate"
+}
+
+CGST_SGST_025_CODE="48565e06-c5be-4e8b-8610-ec44b2a34026"
