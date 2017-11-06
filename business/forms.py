31901ebf0e2fbe2b22cabbb49d503ff6f7b3a176
pythonart <31215688+pythonart@users.noreply.github.com>
2017-11-06 18:01:30 +0530

    Update forms.py

--- business/forms.py
+++ business/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from django.forms import ModelForm 
 from django.core.exceptions import ValidationError
 
-#from .models import  Business, MyProfile
+from .models import  Business
 
 #MONTH_CHOICES=[('JAN','1'),('
 
