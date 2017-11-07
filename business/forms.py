f44621952be466ed70dcc650df1863535784317f
pythonart <31215688+pythonart@users.noreply.github.com>
2017-11-07 11:26:03 +0530

    Update forms.py

--- business/forms.py
+++ business/forms.py
@@ -8,7 +8,7 @@ from accountingbuddy.models import   MyProfile
 from .models import  Business
 
 MONTH_CHOICES=[(str(k),calendar.month_abbr[k]) for k in range(1,13)]
-YEAR_CHOICES==[(str(k),str(k)) for k in range(2015,2051)]
+YEAR_CHOICES=[(str(k),str(k)) for k in range(2015,2051)]
 #[(1, 'Jan'), (2, 'Feb'), (3, 'Mar'), (4, 'Apr'), (5, 'May'), (6, 'Jun'), (7, 'Jul'), (8, 'Aug'), (9, 'Sep'), (10, 'Oct'), (11, 'Nov'), (12, 'Dec')]
 
 class BusinessCreateForm(ModelForm):
