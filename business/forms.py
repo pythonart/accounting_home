28e2c007ee8c1008f7c1249f05ee929e92007073
pythonart <31215688+pythonart@users.noreply.github.com>
2017-11-08 01:16:30 +0530

    Update forms.py

--- business/forms.py
+++ business/forms.py
@@ -33,7 +33,7 @@ class GstOffLineGenForm(forms.Form):
       q=Business.objects.all()
     else:
       select_user=MyProfile.objects.get(user=self.request.user)
-      q=Business.objects.all().filter(user=select_user.user_id)
+      q=Business.objects.all().filter(user=select_user.user)
       #select_user.user_id
    
 
