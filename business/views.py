79b0b8ec8f1efcf86b406a5e9d0c5367a5f7ecdd
pythonart <31215688+pythonart@users.noreply.github.com>
2017-12-10 12:01:44 +0530

    Update views.py

--- business/views.py
+++ business/views.py
@@ -110,8 +110,8 @@ def SalesInvoiceLine(request):
   return render(request,'business/form.html',{'form':form})  
     
 def SalesInvoiceFormSetView(request,pk):
-  salesinvoice=SalesInvoice.objects.get(pk=1)
-  SalesInvoiceLineFormSet=inlineformset_factory(SalesInvoice,SalesInvoiceLine,fields=('SalesInvoice','Description','TaxCode','Qty','Item','Amount'))  
+  salesinvoice=SalesInvoiceMod.objects.get(pk=1)
+  SalesInvoiceLineFormSet=inlineformset_factory(SalesInvoiceMod,SalesInvoiceLineMod,fields=('SalesInvoice','Description','TaxCode','Qty','Item','Amount'))  
   if request.method=="POST":
     formset=SalesInvoiceLineFormSet(request.POST,request.FILES,instance=salesinvoice)
     if formset.is_valid():
