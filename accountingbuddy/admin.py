b2ca8ad190fb39d1dbdffeebeacc6b51ab480018
pythonart <31215688+pythonart@users.noreply.github.com>
2017-12-31 12:27:18 +0530

    Update admin.py

--- accountingbuddy/admin.py
+++ accountingbuddy/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Pricing,  Business_request, SendMails,SupportTypes,Support_request,Advert, DesktopActivationReq
+from .models import Pricing,  Business_request, SendMails,SupportTypes,Support_request,Advert
+from .models import DesktopActivationReq,BusinessTypes,BusinessTypeFile
 
 # Register your models here.
 
@@ -9,6 +10,8 @@ admin.site.register(SendMails)
 admin.site.register(SupportTypes)
 admin.site.register(Advert)
 admin.site.register(DesktopActivationReq)
+admin.site.register(BusinessTypes)
+admin.site.register(BusinessTypeFile)
 
 
 class SupportRequestAdmin(admin.ModelAdmin):
