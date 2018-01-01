599ec6d925e39c1e50b85eee366d12558018e5e8
pythonart <31215688+pythonart@users.noreply.github.com>
2018-01-01 22:12:01 +0530

    Update urls.py

--- business/urls.py
+++ business/urls.py
@@ -13,5 +13,6 @@ url(r'^business/list/$'  , views.BusinessListView.as_view() , name='business-lis
 url(r'^business/gstexport/$', views.GstOffLineView          , name='business-gst-export'),  
 url(r'^business/salesinvoice/$',views.SalesInvoiceCreate,name='sales-invoice'),
 url(r'^business/salesinvline/$',views.SalesInvoiceLine,name='sales-inv-line'),  
-url(r'^business/salesinvformset/(?P<pk>[0-9]+)/$',views.SalesInvoiceFormSetView,name='sales-inv-formset'),  
+url(r'^business/salesinvformset/(?P<pk>[0-9]+)/$',views.SalesInvoiceFormSetView,name='sales-inv-formset'),
+url(r'^business/import/$',views.import_business_view.as_view(),name='business-import'),  
 ]
