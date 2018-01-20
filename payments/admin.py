0848e910c912397a13a7350853661e186d0e1015
pythonart <31215688+pythonart@users.noreply.github.com>
2018-01-21 00:43:46 +0530

    Update admin.py

--- payments/admin.py
+++ payments/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Invoice,Agent,SubscriptionType,Subscription,
 
 # Register your models here.
+
+admin.site.register(Invoice)
+admin.site.register(Agent)
+admin.site.register(SubscriptionType)
+admin.site.register(Subscription)
