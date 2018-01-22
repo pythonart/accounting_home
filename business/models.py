295713855346df40b136156bc9f0427cacea6307
pythonart <31215688+pythonart@users.noreply.github.com>
2018-01-23 01:24:10 +0530

    Update models.py

--- business/models.py
+++ business/models.py
@@ -2,14 +2,23 @@ from django.db import models
 
 # Create your models here.
 
+
+class Server(models.Model):
+  url=models.URLField('Server Address',max_length=200)
+  
+  def __str__(self):
+    return self.url
+
 class Business(models.Model):
   name=models.CharField("Business Name",max_length=50)
   code=models.CharField("Code",max_length=200)
   user=models.ForeignKey("auth.User")
   created = models.DateTimeField(auto_now_add=True)
-
+  #url=models.ForeignKey(Server,blank=True,null=True)
+  
   def __str__(self):
     return "%s" % self.name
+    
   
 class SalesInvoiceMod(models.Model):
   IssueDate=models.DateField('Invoice Date')
