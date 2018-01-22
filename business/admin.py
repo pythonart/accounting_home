98d7171735a21eb626c0fb2be394b01c7c28ab29
pythonart <31215688+pythonart@users.noreply.github.com>
2018-01-23 04:20:45 +0530

    Update admin.py

--- business/admin.py
+++ business/admin.py
@@ -7,7 +7,7 @@ from .models import  Business, Server
 class BusinessAdmin(admin.ModelAdmin):
   list_display=('id','name','code','user','created','url')
   list_filter=('id','name','code','user','created','url')
-  search_fields=('name','code','user','created')
+  search_fields=('name','code','user')
   ordering=('name',)
   
   
